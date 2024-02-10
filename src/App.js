c262489dccb5f988dbdd2904e3eae9ffff7219a4
Syed Huzaifa Nazim <142250085+SyedHuzaifaNazim@users.noreply.github.com>
2024-02-11 00:51:54 +0500

    For SMIT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const ListItem = (props) => (
+  <li onClick={() => props.handleClick(props.name)}>{props.name}</li>
+);
+
+const ListDisplay = (props) => {
+  const items = props.items.map((item, i) => (
+    <ListItem key={i} name={item} handleClick={props.handleClick} />
+  ));
+  return <ul>{items}</ul>;
+};
+
+const InputText = (props) => {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.handleSubmit(value);
+    setValue('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+    </form>
+  );
+};
+
+function App() {
+  const [items, setItems] = useState(['oranges', 'apples', 'candy']);
+
+  const handleClick = (item) => {
+    setItems(items.filter((i) => i !== item));
+  };
+
+  const handleSubmit = (item) => {
+    setItems(items.concat(item));
+  };
+
+  return (
+    <div id="list-container">
+      <h1>Shopping List</h1>
+      <ListDisplay items={items} handleClick={handleClick} />
+      <InputText handleSubmit={handleSubmit} />
+      {/* <form> */}
+        {/* <InputText handleSubmit={handleClick}/> */}
+      {/* </form> */}
     </div>
   );
 }
---
+++ src/xoft.js
@@ -0,0 +1,11 @@
+import  React  from "react";
+import ReactDOM from "react-dom";
+
+function xoft(){
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+export default xoft;
